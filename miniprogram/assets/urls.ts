20624537b1cbd5d6f7da638b07b3bf14368a8869
trv3wood <141489188+trv3wood@users.noreply.github.com>
2026-02-28 23:22:16 +0800

    asset常量

---
+++ miniprogram/assets/urls.ts
@@ -0,0 +1,65 @@
+/**
+ * 静态资源 URL 常量定义
+ */
+
+export const ASSETS = {
+    /** 个人中心相关的资源 */
+    PROFILE: {
+        /** 个人中心-名片 */
+        CARD: 'https://kuaizu-img-file.oss-cn-hangzhou.aliyuncs.com/kuaizu-file/assets/%E4%B8%AA%E4%BA%BA%E4%B8%AD%E5%BF%83-%E5%90%8D%E7%89%87.svg',
+        /** 个人中心-我的客服 */
+        CUSTOMER_SERVICE: 'https://kuaizu-img-file.oss-cn-hangzhou.aliyuncs.com/kuaizu-file/assets/%E4%B8%AA%E4%BA%BA%E4%B8%AD%E5%BF%83-%E6%88%91%E7%9A%84%E5%AE%A2%E6%9C%8D.svg',
+        /** 个人中心-未认证标识 */
+        UNVERIFIED: 'https://kuaizu-img-file.oss-cn-hangzhou.aliyuncs.com/kuaizu-file/assets/%E4%B8%AA%E4%BA%BA%E4%B8%AD%E5%BF%83-%E6%9C%AA%E8%AE%A4%E8%AF%81%E6%A0%87%E8%AF%86.svg',
+        /** 个人中心-橄榄枝 */
+        OLIVE_BRANCH: 'https://kuaizu-img-file.oss-cn-hangzhou.aliyuncs.com/kuaizu-file/assets/%E4%B8%AA%E4%BA%BA%E4%B8%AD%E5%BF%83-%E6%A9%84%E6%A6%84%E6%9E%9D.svg',
+        /** 个人中心-空头像 */
+        EMPTY_AVATAR: 'https://kuaizu-img-file.oss-cn-hangzhou.aliyuncs.com/kuaizu-file/assets/%E4%B8%AA%E4%BA%BA%E4%B8%AD%E5%BF%83-%E7%A9%BA%E5%A4%B4%E5%83%8F.svg',
+        /** 个人中心-背景 */
+        BACKGROUND: 'https://kuaizu-img-file.oss-cn-hangzhou.aliyuncs.com/kuaizu-file/assets/%E4%B8%AA%E4%BA%BA%E4%B8%AD%E5%BF%83-%E8%83%8C%E6%99%AF.svg',
+        /** 个人中心-订单中心 */
+        ORDER_CENTER: 'https://kuaizu-img-file.oss-cn-hangzhou.aliyuncs.com/kuaizu-file/assets/%E4%B8%AA%E4%BA%BA%E4%B8%AD%E5%BF%83-%E8%AE%A2%E5%8D%95%E4%B8%AD%E5%BF%83.svg',
+        /** 个人中心-认证成功标识 */
+        VERIFIED_SUCCESS: 'https://kuaizu-img-file.oss-cn-hangzhou.aliyuncs.com/kuaizu-file/assets/%E4%B8%AA%E4%BA%BA%E4%B8%AD%E5%BF%83-%E8%AE%A4%E8%AF%81%E6%88%90%E5%8A%9F%E6%A0%87%E8%AF%86.svg',
+        /** 个人中心-设置按钮 */
+        SETTING_BTN: 'https://kuaizu-img-file.oss-cn-hangzhou.aliyuncs.com/kuaizu-file/assets/%E4%B8%AA%E4%BA%BA%E4%B8%AD%E5%BF%83-%E8%AE%BE%E7%BD%AE%E6%8C%89%E9%92%AE.svg',
+        /** 个人中心-资讯中心 */
+        INFO_CENTER: 'https://kuaizu-img-file.oss-cn-hangzhou.aliyuncs.com/kuaizu-file/assets/%E4%B8%AA%E4%BA%BA%E4%B8%AD%E5%BF%83-%E8%B5%84%E8%AE%AF%E4%B8%AD%E5%BF%83.svg',
+        /** 个人中心-进入 */
+        ENTER: 'https://kuaizu-img-file.oss-cn-hangzhou.aliyuncs.com/kuaizu-file/assets/%E4%B8%AA%E4%BA%BA%E4%B8%AD%E5%BF%83-%E8%BF%9B%E5%85%A5.svg',
+    },
+
+    /** 首页相关的资源 */
+    HOME: {
+        /** 首页-背景 */
+        BACKGROUND: 'https://kuaizu-img-file.oss-cn-hangzhou.aliyuncs.com/kuaizu-file/assets/%E9%A6%96%E9%A1%B5-%E8%83%8C%E6%99%AF.svg',
+        /** 首页-校园人才-人才个数 */
+        TALENT_COUNT_BG: 'https://kuaizu-img-file.oss-cn-hangzhou.aliyuncs.com/kuaizu-file/assets/%E9%A6%96%E9%A1%B5-%E6%A0%A1%E5%9B%AD%E4%BA%BA%E6%89%8D-%E4%BA%BA%E6%89%8D%E4%B8%AA%E6%95%B0.png',
+        /** 首页-校园人才 */
+        TALENT_ICON: 'https://kuaizu-img-file.oss-cn-hangzhou.aliyuncs.com/kuaizu-file/assets/%E9%A6%96%E9%A1%B5-%E6%A0%A1%E5%9B%AD%E4%BA%BA%E6%89%8D.svg',
+        /** 首页-校园项目-放大镜 */
+        PROJECT_SEARCH_ICON: 'https://kuaizu-img-file.oss-cn-hangzhou.aliyuncs.com/kuaizu-file/assets/%E9%A6%96%E9%A1%B5-%E6%A0%A1%E5%9B%AD%E9%A1%B9%E7%9B%AE-%E6%94%BE%E5%A4%A7%E9%95%9C.png',
+        /** 首页-校园项目 */
+        PROJECT_ICON: 'https://kuaizu-img-file.oss-cn-hangzhou.aliyuncs.com/kuaizu-file/assets/%E9%A6%96%E9%A1%B5-%E6%A0%A1%E5%9B%AD%E9%A1%B9%E7%9B%AE.svg',
+    },
+
+    /** Tab Bar 按钮 */
+    TAB_BAR: {
+        /** 个人中心按钮-已激活 */
+        PROFILE_ACTIVE: 'https://kuaizu-img-file.oss-cn-hangzhou.aliyuncs.com/kuaizu-file/assets/%E4%B8%AA%E4%BA%BA%E4%B8%AD%E5%BF%83%E6%8C%89%E9%92%AE-%E5%B7%B2%E6%BF%80%E5%8F%91.svg',
+        /** 个人中心按钮-未激活 */
+        PROFILE_INACTIVE: 'https://kuaizu-img-file.oss-cn-hangzhou.aliyuncs.com/kuaizu-file/assets/%E4%B8%AA%E4%BA%BA%E4%B8%AD%E5%BF%83%E6%8C%89%E9%92%AE-%E6%9C%AA%E6%BF%80%E5%8F%91.svg',
+        /** 首页按钮-未激活 */
+        HOME_INACTIVE: 'https://kuaizu-img-file.oss-cn-hangzhou.aliyuncs.com/kuaizu-file/assets/%E9%A6%96%E9%A1%B5%E6%8C%89%E9%92%AE-%E6%9C%AA%E6%BF%80%E5%8F%91.svg',
+    },
+
+    /** 通用静态资源 */
+    COMMON: {
+        /** 发布按钮 */
+        PUBLISH_BTN: 'https://kuaizu-img-file.oss-cn-hangzhou.aliyuncs.com/kuaizu-file/assets/%E5%8F%91%E5%B8%83%E6%8C%89%E9%92%AE.svg',
+        /** 我的项目图标 */
+        MY_PROJECT_ICON: 'https://kuaizu-img-file.oss-cn-hangzhou.aliyuncs.com/kuaizu-file/assets/%E6%88%91%E7%9A%84%E9%A1%B9%E7%9B%AE%E5%9B%BE%E6%A0%87.png',
+    }
+};
+
+export default ASSETS;
