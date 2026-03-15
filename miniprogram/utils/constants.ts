fa9b117362188a9313ea8f163b0442b1fe7d52a9
trv3wood <141489188+trv3wood@users.noreply.github.com>
2026-03-15 21:08:27 +0800

    feat: BizKey constants

---
+++ miniprogram/utils/constants.ts
@@ -0,0 +1,12 @@
+// constants.ts
+
+// Message Business Keys (Subscription Messages)
+export const MsgBizKey = {
+    CardReceived: "MSG_CARD_RECEIVED",
+    CardDeliveryResult: "MSG_CARD_DELIVERY_RESULT",
+    AuditResultProj: "MSG_AUDIT_RESULT_PROJ",
+    UserReply: "MSG_USER_REPLY",
+    InviteJoin: "MSG_INVITE_JOIN",
+    AuditResultUser: "MSG_AUDIT_RESULT_USER",
+    IdentityAuth: "MSG_IDENTITY_AUTH"
+}
